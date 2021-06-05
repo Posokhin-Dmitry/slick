da56533c15e155054b7061dc457dc6a96a88cbe7
Dmitry Posokhin <79544402+NEGANN@users.noreply.github.com>
2021-06-06 00:56:06 +0300

    залил слайдер

---
+++ README.md
@@ -0,0 +1,3 @@
+# goit-markup-hw-08
+
+https://posokhin-dmitry.github.io/goit-markup-hw-08/
---
+++ js/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('.slider').slick({
+    arrows: false,
+    dots: true,
+    adaptiveHeight: true,
+    easing: 'easeInOutQuint',
+    autoplay: true,
+    autoplaySpeed: 3000,
+  })
+})
